33c0223d5549a3a25cc802d04f6a8fb336c51df1
Ilya <ilak1856@gmail.com>
2023-06-01 18:42:27 +0300

    Add angry pose and update enemys dmg

--- Assets/Skripts/Player/States/IdleState.cs
+++ Assets/Skripts/Player/States/IdleState.cs
@@ -1,7 +1,24 @@
+using UnityEngine;
+
 public class IdleState : PlayerState
 {
+
+    [SerializeField] private float _timeout;
+
+    private float _waitTime;
+
+    private void OnEnable()
+    {
+        _waitTime = 0;
+    }
     private void Update()
     {
+        _waitTime += Time.deltaTime;
 
+        if( _waitTime >= _timeout )
+        {
+            Animator.SetTrigger("Cute");
+            _waitTime = 0;
+        }
     }
 }
